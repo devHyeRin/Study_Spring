928cf115f5355a0c25e38c0086b049dcff2515ae
Hyerin Lee <hyerinlee0v0@gmail.com>
2023-10-27 01:49:02 +0900

    db commit

--- src/main/java/study/studyspring/controller/MemberController.java
+++ src/main/java/study/studyspring/controller/MemberController.java
@@ -2,10 +2,13 @@ package study.studyspring.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import study.studyspring.domain.Member;
 import study.studyspring.service.MemberService;
+
+import java.util.List;
 
 @Controller
 public class MemberController {
@@ -25,10 +28,19 @@ public class MemberController {
     @PostMapping("/members/new")
     public String create(MemberForm form){
         Member member = new Member();
-        member.setName(form.getName());
+        member.setName(form.getName());       // private로 함부로 접근하지 못하도록 함
+
+        System.out.println("member = " + member.getName());   // member가 잘넘어오는지 출력해보기!
 
         memberService.join(member);    // 회원가입
 
         return "redirect:/";
+    }
+
+    @GetMapping("/members")
+    public String list(Model model){
+        List<Member> members = memberService.findMembers();
+        model.addAttribute("members", members);
+        return "members/memberlist";
     }
 }
